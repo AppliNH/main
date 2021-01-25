cf39844a577f741e6f925a039b6b203b593f62b9
AppliNH <thomas.martindev@gmail.com>
2021-01-25 14:51:41 +0100

    including working example https://github.com/getcouragenow/main/issues/109

--- installCurl.sh
+++ installCurl.sh
@@ -3,6 +3,11 @@
 # Grab the binaries attached as release assets
 curl -s https://api.github.com/repos/getcouragenow/main/releases/latest | jq -r ".assets[] | .browser_download_url" | xargs wget
 
+# WORKING EXAMPLE :
+curl -s https://api.github.com/repos/NFSTools/Binary/releases/latest | jq -r ".assets[] | .browser_download_url"  | xargs wget
+zipfilename="$(curl -s https://api.github.com/repos/NFSTools/Binary/releases/latest | jq -r '.assets[].name')"
+unzip -j $zipfilename -d binaries
+
 # Get the code as tar.gz, decompress..
 curl -L "$(curl -s https://api.github.com/repos/applinh-getcouragenow/main/releases/latest | jq -r ".tarball_url")" > main.tar.gz 
 chmod 700 main.tar.gz
